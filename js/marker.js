336bbab8c1ce31818b4839e3c122cd2cb6b80c70
thuy616 <nguyen.n.t.thuy@gmail.com>
2015-10-13 01:47:26 +0300

    Fixed Markers

--- js/marker.js
+++ js/marker.js
@@ -17,29 +17,15 @@ var Markers = {
 
         l.text = text;
         l.v = vector;
-        //l.text.style.top = vector.y + 'px';
-        //l.text.style.left = vector.x + 'px' ;
-        //l.text.style.zIndex = vector.z;
 
         this.markers.push(l);
 
         document.getElementById("container").appendChild(l.text);
         //to2DPlane(l, vector, camera);
         return l.text;
-    },
+    }
 
-    toXYCoords: function(pos, camera) {
-        var vector = pos.clone();
-        vector.project(camera);
-        // map to 2D screen space
-        vector.x = Math.round( (   vector.x + 1 ) * window.innerWidth  / 2 );
-        vector.y = Math.round( ( - vector.y + 1 ) * window.innerHeight / 2 );
-        return vector;
-    },
-
-
-    update: function(camera){
-
+<<<<<<< Updated upstream
         for (var i=0; i<this.markers.length; i++) {
             var item = this.markers[i];
             var newPos = this.toXYCoords(item.v, camera);
@@ -52,20 +38,16 @@ var Markers = {
             }
         }
     }
+=======
+>>>>>>> Stashed changes
 };
 
-var to2DPlane = function(marker, v3, camera) {
-    var vector = v3.clone();
+var toXYCoords = function(pos, camera) {
+    var vector = pos.clone();
     vector.project(camera);
     // map to 2D screen space
     vector.x = Math.round( (   vector.x + 1 ) * window.innerWidth  / 2 );
     vector.y = Math.round( ( - vector.y + 1 ) * window.innerHeight / 2 );
-    marker.text.style.top  = vector.y + 'px';
-    marker.text.style.left = vector.x + 'px';
-    if(vector.z > 1.0){
-        marker.text.style.display = 'none';
-    } else {
-        marker.text.style.display = 'inline-block';
-    }
+    return vector;
 };
 
