2d781908ba81300ba756fa17c68762ecf2e98f0e
thuy616 <nguyen.n.t.thuy@gmail.com>
2015-10-13 01:42:39 +0300

    Fixed Markers

--- js/marker.js
+++ js/marker.js
@@ -17,55 +17,39 @@ var Markers = {
 
         l.text = text;
         l.v = vector;
-        //l.text.style.top = vector.y + 'px';
-        //l.text.style.left = vector.x + 'px' ;
-        //l.text.style.zIndex = vector.z;
 
         this.markers.push(l);
 
         document.getElementById("container").appendChild(l.text);
         //to2DPlane(l, vector, camera);
         return l.text;
-    },
+    }
 
-    toXYCoords: function(pos, camera) {
-        var vector = pos.clone();
-        vector.project(camera);
-        // map to 2D screen space
-        vector.x = Math.round( (   vector.x + 1 ) * window.innerWidth  / 2 );
-        vector.y = Math.round( ( - vector.y + 1 ) * window.innerHeight / 2 );
-        return vector;
-    },
-
-
-    update: function(camera){
-
-        for (var i=0; i<this.markers.length; i++) {
-            var item = this.markers[i];
-            var newPos = this.toXYCoords(item.v, camera);
-            item.text.style.top  = newPos.y + 'px';
-            item.text.style.left = newPos.x + 'px';
-            if(newPos.z > 1.0){
-                item.text.style.display = 'none';
-            } else {
-                item.text.style.display = 'inline-block';
-            }
-        }
-    },
 };
 
-var to2DPlane = function(marker, v3, camera) {
-    var vector = v3.clone();
+var toXYCoords = function(pos, camera) {
+    var vector = pos.clone();
     vector.project(camera);
     // map to 2D screen space
     vector.x = Math.round( (   vector.x + 1 ) * window.innerWidth  / 2 );
     vector.y = Math.round( ( - vector.y + 1 ) * window.innerHeight / 2 );
-    marker.text.style.top  = vector.y + 'px';
-    marker.text.style.left = vector.x + 'px';
-    if(vector.z > 1.0){
-        marker.text.style.display = 'none';
-    } else {
-        marker.text.style.display = 'inline-block';
-    }
+    return vector;
 };
+
+var updateMarkers = function(markers, camera){
+
+    for (var i=0; i<markers.length; i++) {
+        var item = markers[i];
+        var newPos = this.toXYCoords(item.v, camera);
+        item.text.style.top  = newPos.y + 'px';
+        item.text.style.left = newPos.x + 'px';
+        if(newPos.z > 1.0){
+            item.text.style.display = 'none';
+        } else {
+            item.text.style.display = 'inline-block';
+        }
+    }
+}
+
+
 
